8e2fd78836c3bc9ffea043b916f493a02f35c362
adelahera <dlhlalejandro@gmail.com>
2023-10-20 10:03:48 +0200

    Creada entidad AsignacionPedidos donde se desarrollará la lógica de negocio
    Esta es la clase donde se desarrollará la lógica de negocio, y por ello, reúne una lista de envíos, y una lista de camiones (todos los camiones y envíos disponibles para esa semana). Además, se incluye la matriz de distancias, que se incluye también en la parte de la resolución de la lógica de negocio

--- src/AsignacionPedidos.ts
+++ src/AsignacionPedidos.ts
@@ -1,2 +1,17 @@
+import { Camion } from "./Camion";
+import { Envio } from "./Envio";
 
 export declare var maximoEnvios: number;
+
+class AsignacionPedidos{
+
+    camiones: Array<Camion>;
+    envios: Array<Envio>;
+    distanciaEntreEnvios: number[][];
+
+    constructor(camiones: Array<Camion>, envios: Array<Envio>, distanciaEntreEnvios: number[][]){
+        this.camiones = camiones;
+        this.envios = envios;
+        this.distanciaEntreEnvios = distanciaEntreEnvios;
+    }
+}
