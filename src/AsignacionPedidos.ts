561e42bc4e459d53152b16f1974446b04e032767
adelahera <dlhlalejandro@gmail.com>
2023-10-20 09:45:31 +0200

    Los camiones tendrán un máximo de envíos a realizar closes #9
    Se ha creado una variable global donde se guardará el número de envíos máximos que podrá hacer cada camión

--- src/AsignacionPedidos.ts
+++ src/AsignacionPedidos.ts
@@ -1,15 +1,2 @@
-import { Camion } from "./Camion";
-import { Envio } from "./Envio";
 
-class AsignacionPedidos{
-
-    camiones: Array<Camion>;
-    envios: Array<Envio>;
-    distanciaEntreEnvios: Array<Array<number>>;
-
-    constructor(camiones: Array<Camion>, envios: Array<Envio>, distanciaEntreEnvios: Array<Array<number>>){
-        this.camiones = camiones;
-        this.envios = envios;
-        this.distanciaEntreEnvios = distanciaEntreEnvios;
-    }
-}
+export declare var maximoEnvios: number;
