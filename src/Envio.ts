d89a54ea6e977710d666fe75ea2fc5f309606eb8
adelahera <dlhlalejandro@gmail.com>
2023-10-20 09:26:12 +0200

    Los destinos de cada envío pueden ser diferentes closes #10
    He decidido que los destinos de cada envío pueden ser diferentes para cada envío, pues creo que si no fuera de esta manera, no tendría sentido la lógica de negocio en sí, al tener que ir siempre a los mismo lugares

--- src/Envio.ts
+++ src/Envio.ts
@@ -1,14 +1,4 @@
 export class Envio {
 
     destino: string;
-    carga: number;
-    consumo: number;
-    listaDias: Array<Date>;
-
-    constructor(destino: string, carga: number, consumo: number, listaDias: Array<Date>) {
-        this.destino = destino;
-        this.carga = carga;
-        this.consumo = consumo;
-        this.listaDias = listaDias;
-    }
 }
