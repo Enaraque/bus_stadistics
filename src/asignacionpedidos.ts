7ec61d92de5b7b381c2d5b21d582202e6014723a
Enrique Araque <minixute@gmail.com>
2023-11-19 12:20:25 +0100

    Añadida almacenamiento de datos closes #23 Se ha diseñado la logica para poder extraer los datos del json y asignarlos a cada objeto valor

--- src/asignacionpedidos.ts
+++ src/asignacionpedidos.ts
@@ -1,5 +1,6 @@
 import { Camion } from "./camion.ts";
 import { Envio } from "./envio.ts";
+import json from "./data/reparto.json" with { type: "json" };
 
 
 export type DistanciaEntreDestinos = { 
@@ -8,15 +9,54 @@ export type DistanciaEntreDestinos = {
     distancia: number;
 }
 
-class AsignacionPedidos{
+export class AsignacionPedidos{
 
     camiones: Camion[];
     envios: Envio[];
     distancias: DistanciaEntreDestinos[];
 
-    constructor(camiones: Camion[], envios: Envio[], distancias: DistanciaEntreDestinos[]){
-        this.camiones = camiones;
-        this.envios = envios;
-        this.distancias = distancias;
+    constructor() {
+        const {camiones, envios, distanciaEntreEnvios} = json;
+        this.camiones = this.extraer_camiones_json(camiones);
+        this.envios = this.extraer_envios_json(envios);
+        this.distancias = this.extraer_distancias_json(distanciaEntreEnvios);
     }
+
+    private parseFechaDDMMYY(dateString: string): Date {
+        const [day, month, year] = dateString.split("-").map(Number);
+        return new Date(year, month-1, day+1);
+    }
+
+    extraer_camiones_json(camiones : number[]): Camion[] {
+        return camiones.map((cargaMaxima: number) => new Camion(cargaMaxima));
+    }
+
+    extraer_envios_json(envios : {destino: string, carga : string, consumo: string, listaDias: string[]}[]): Envio[] {
+        const array_envios : Envio[] = [];
+        envios.forEach((value) => {
+            const {destino} = value;
+            const carga = parseInt(value.carga);
+            const consumo = parseInt(value.consumo);
+            const listaDias = value.listaDias.map(dateString => this.parseFechaDDMMYY(dateString));
+
+            const envio = new Envio(destino, carga, consumo, listaDias);
+
+            array_envios.push(envio);
+        });
+
+        return array_envios;
+    };
+
+    extraer_distancias_json(distancias : {origen: string, destino: string, distancia: string}[]): DistanciaEntreDestinos[] {
+        const array_distancias : DistanciaEntreDestinos[] = [];
+        distancias.forEach(function (value) {
+            const {origen, destino} = value;
+            const distancia = parseInt(value.distancia);
+            const distancia_entre_envios_actual : DistanciaEntreDestinos = {origen, destino, distancia};
+            array_distancias.push(distancia_entre_envios_actual);
+        });
+
+        return array_distancias;
+    }
+
 }
